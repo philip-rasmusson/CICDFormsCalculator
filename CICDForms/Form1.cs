5dc1839a259847aa9252c19a0f53fa2fdf587d1b
Marcus Sjöholm <marcusasjoholm93@gmail.com>
2021-10-28 16:04:46 +0200

    Rewrote calculations

--- CICDForms/Form1.cs
+++ CICDForms/Form1.cs
@@ -12,197 +12,160 @@
 
     public partial class Form1 : Form
     {
-        private double FirstNumber;
-        private string Operation;
-
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Button_0_Click(object sender, EventArgs e)
-        {
-            RealOutputBox.Text = RealOutputBox.Text + "0";
-        }
+        private double FirstNumber { get; set; }
+        private string Operation { get; set; }
+        private double Result { get; set; }
+
+        private void Button_0_Click(object sender, EventArgs e) => RealOutputBox.Text += "0";
 
         private void Button_1_Click(object sender, EventArgs e)
         {
-            if (RealOutputBox.Text == "0" && RealOutputBox.Text != null)
-            {
+            RealOutputBox.Text = "";
+            if (RealOutputBox.Text is "0" and not null)
                 RealOutputBox.Text = "1";
-            }
             else
-            {
-                RealOutputBox.Text = RealOutputBox.Text + "1";
-            }
+                RealOutputBox.Text += "1";
         }
 
         private void Button_2_Click(object sender, EventArgs e)
         {
-            if (RealOutputBox.Text == "0" && RealOutputBox.Text != null)
-            {
+            RealOutputBox.Text = "";
+            if (RealOutputBox.Text is "0" and not null)
                 RealOutputBox.Text = "2";
-            }
             else
-            {
-                RealOutputBox.Text = RealOutputBox.Text + "2";
-            }
+                RealOutputBox.Text += "2";
         }
 
         private void Button_3_Click(object sender, EventArgs e)
         {
-            if (RealOutputBox.Text == "0" && RealOutputBox.Text != null)
-            {
+            RealOutputBox.Text = "";
+            if (RealOutputBox.Text is "0" and not null)
                 RealOutputBox.Text = "3";
-            }
             else
-            {
-                RealOutputBox.Text = RealOutputBox.Text + "3";
-            }
+                RealOutputBox.Text += "3";
         }
 
         private void Button_4_Click(object sender, EventArgs e)
         {
-            if (RealOutputBox.Text == "0" && RealOutputBox.Text != null)
-            {
+            RealOutputBox.Text = "";
+            if (RealOutputBox.Text is "0" and not null)
                 RealOutputBox.Text = "4";
-            }
             else
-            {
-                RealOutputBox.Text = RealOutputBox.Text + "4";
-            }
+                RealOutputBox.Text += "4";
         }
 
         private void Button_5_Click(object sender, EventArgs e)
         {
-            if (RealOutputBox.Text == "0" && RealOutputBox.Text != null)
-            {
+            RealOutputBox.Text = "";
+            if (RealOutputBox.Text is "0" and not null)
                 RealOutputBox.Text = "5";
-            }
             else
-            {
-                RealOutputBox.Text = RealOutputBox.Text + "5";
-            }
+                RealOutputBox.Text += "5";
         }
 
         private void Button_6_Click(object sender, EventArgs e)
         {
-            if (RealOutputBox.Text == "0" && RealOutputBox.Text != null)
-            {
+            RealOutputBox.Text = "";
+            if (RealOutputBox.Text is "0" and not null)
                 RealOutputBox.Text = "6";
-            }
             else
-            {
-                RealOutputBox.Text = RealOutputBox.Text + "6";
-            }
+                RealOutputBox.Text += "6";
         }
 
         private void Button_7_Click(object sender, EventArgs e)
         {
-            if (RealOutputBox.Text == "0" && RealOutputBox.Text != null)
-            {
+            RealOutputBox.Text = "";
+            if (RealOutputBox.Text is "0" and not null)
                 RealOutputBox.Text = "7";
-            }
             else
-            {
-                RealOutputBox.Text = RealOutputBox.Text + "7";
-            }
+                RealOutputBox.Text += "7";
         }
 
         private void Button_8_Click(object sender, EventArgs e)
         {
-            if (RealOutputBox.Text == "0" && RealOutputBox.Text != null)
-            {
+            RealOutputBox.Text = "";
+            if (RealOutputBox.Text is "0" and not null)
                 RealOutputBox.Text = "8";
-            }
             else
-            {
-                RealOutputBox.Text = RealOutputBox.Text + "8";
-            }
+                RealOutputBox.Text += "8";
         }
 
         private void Button_9_Click(object sender, EventArgs e)
         {
-            if (RealOutputBox.Text == "" && RealOutputBox.Text != null)
-            {
+            RealOutputBox.Text = "";
+            if (RealOutputBox.Text is "0" and not null)
                 RealOutputBox.Text = "9";
-            }
             else
-            {
-                RealOutputBox.Text = RealOutputBox.Text + "9";
-            }
+                RealOutputBox.Text += "9";
         }
 
-        private void Button_Clear_Click(object sender, EventArgs e)
-        {
-            RealOutputBox.Text = "0";
-        }
+        private void Button_Clear_Click(object sender, EventArgs e) => RealOutputBox.Text = "0";
 
         private void Button_Division_Click(object sender, EventArgs e)
         {
             FirstNumber = Convert.ToDouble(RealOutputBox.Text);
-            RealOutputBox.Text = "0";
+            RealOutputBox.Text = "/";
             Operation = "/";
         }
 
         private void Button_Equals_Click(object sender, EventArgs e)
         {
-            double SecondNumber;
-            double Result;
-
-            SecondNumber = Convert.ToDouble(RealOutputBox.Text);
-
             if (Operation == "+")
             {
-                Result = (FirstNumber + SecondNumber);
+                Result = FirstNumber + Convert.ToDouble(RealOutputBox.Text);
                 RealOutputBox.Text = Convert.ToString(Result);
                 FirstNumber = Result;
             }
             if (Operation == "-")
             {
-                Result = (FirstNumber - SecondNumber);
+                Result = FirstNumber - Convert.ToDouble(RealOutputBox.Text);
                 RealOutputBox.Text = Convert.ToString(Result);
                 FirstNumber = Result;
             }
             if (Operation == "*")
             {
-                Result = (FirstNumber * SecondNumber);
+                Result = FirstNumber * Convert.ToDouble(RealOutputBox.Text);
                 RealOutputBox.Text = Convert.ToString(Result);
                 FirstNumber = Result;
             }
             if (Operation == "/")
             {
-                if (SecondNumber == 0)
-                {
+#pragma warning disable RCS1003 // Add braces to if-else (when expression spans over multiple lines).
+                if (Convert.ToDouble(RealOutputBox.Text) == 0)
                     RealOutputBox.Text = "Cannot divide by zero";
-                }
                 else
                 {
-                    Result = (FirstNumber / SecondNumber);
+                    Result = FirstNumber / Convert.ToDouble(RealOutputBox.Text);
                     RealOutputBox.Text = Convert.ToString(Result);
                     FirstNumber = Result;
                 }
+#pragma warning restore RCS1003 // Add braces to if-else (when expression spans over multiple lines).
             }
         }
 
         private void Button_Minus_Click(object sender, EventArgs e)
         {
             FirstNumber = Convert.ToDouble(RealOutputBox.Text);
-            RealOutputBox.Text = "0";
+            RealOutputBox.Text = "-";
             Operation = "-";
         }
 
         private void Button_Multiplication_Click(object sender, EventArgs e)
         {
             FirstNumber = Convert.ToDouble(RealOutputBox.Text);
-            RealOutputBox.Text = "0";
+            RealOutputBox.Text = "*";
             Operation = "*";
         }
 
         private void Button_Plus_Click(object sender, EventArgs e)
         {
-            FirstNumber = Convert.ToDouble(this.RealOutputBox.Text);
-            RealOutputBox.Text = "0";
+            FirstNumber = Convert.ToDouble(RealOutputBox.Text);
+            RealOutputBox.Text = "+";
             Operation = "+";
         }
     }
